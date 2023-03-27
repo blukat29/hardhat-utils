8988e021f2802dcd71265e90f40c505ff4d2f119
ollie.j <ollie.j@krustuniverse.com>
2023-03-27 19:06:38 +0900

    Add tasks call and send

--- src/deployments.ts
+++ src/deployments.ts
@@ -1,12 +1,63 @@
 import { task } from "hardhat/config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { FromArgType, resolveFuncArgs, normalizeCallResult, normalizeRpcResult } from "./helpers";
 
 import "./type-extensions";
 
 export const TASK_ADDR = "addr";
+export const TASK_CALL = "call";
+export const TASK_SEND = "send";
 
 task(TASK_ADDR, "Get address of a deployed contract")
   .addPositionalParam("name", "Contract name")
-  .setAction(async ({ name }, hre) => {
+  .setAction(async (taskArgs, hre: HardhatRuntimeEnvironment) => {
+    const { name } = taskArgs;
+
     const deployment = await hre.deployments.get(name);
     console.log(deployment.address);
+  });
+
+// TODO: call and send: --gas-price, --gas-limit, --nonce
+task(TASK_CALL, "Call a read-only function to a contract")
+  .addOptionalParam("from", "Caller address or index", 0, FromArgType)
+  .addOptionalParam("to", "Target address. Loaded from deployments if empty.", "")
+  .addFlag("raw", "Print raw output")
+  .addFlag("dec", "Print numbers in decimal (default is hex)")
+  .addPositionalParam("name", "Contract name (example: 'Counter', 'src/Lock.sol:Lock')")
+  .addPositionalParam("func", "Function name or signature (example: 'number()', 'balanceOf(address)')")
+  .addVariadicPositionalParam("args", "call arguments", [])
+  .setAction(async (taskArgs, hre: HardhatRuntimeEnvironment) => {
+    const { func, raw, dec } = taskArgs;
+    const { contract, sender, unsignedTx } = await resolveFuncArgs(taskArgs, hre);
+
+    let output = await sender.call(unsignedTx);
+    if (raw) {
+      console.log(output);
+    } else {
+      let res = contract.interface.decodeFunctionResult(func, output);
+      console.log(normalizeCallResult(res, { dec }));
+    }
+  });
+
+task(TASK_SEND, "Send a transaction to a contract")
+  .addOptionalParam("from", "Caller address or index", 0, FromArgType)
+  .addOptionalParam("to", "Target address. Loaded from deployments if empty.", "")
+  .addFlag("unsigned", "Print unsigned tx and exit")
+  .addFlag("dec", "Print numbers in decimal (default is hex)")
+  .addPositionalParam("name", "Contract name (example: 'Counter', 'src/Lock.sol:Lock')")
+  .addPositionalParam("func", "Function name or signature (example: 'number()', 'balanceOf(address)')")
+  .addVariadicPositionalParam("args", "call arguments", [])
+  .setAction(async (taskArgs, hre: HardhatRuntimeEnvironment) => {
+		const { unsigned, dec } = taskArgs;
+		const { sender, unsignedTx } = await resolveFuncArgs(taskArgs, hre);
+
+		if (unsigned) {
+			console.log(normalizeRpcResult(unsignedTx, { dec }));
+			return;
+		}
+
+    // TODO: decode events
+		let tx = await sender.sendTransaction(unsignedTx);
+		let rc = await tx.wait();
+		console.log(normalizeRpcResult(rc, { dec }));
   });
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,6 @@
+import type ethers from "ethers";
+import { BigNumber } from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
 import _ from "lodash";
 
 export class FromArgType {
@@ -10,4 +13,75 @@ export class FromArgType {
       return strValue;
     }
   }
+}
+
+export interface FuncTaskCommonArgs {
+  name: string;
+  func: string;
+  args: string[];
+  from: string;
+  to: string;
+}
+
+interface ResolvedFuncArgs {
+  contract: ethers.Contract;
+  sender: ethers.Signer;
+  unsignedTx: any;
+}
+
+export async function resolveFuncArgs(
+  taskArgs: FuncTaskCommonArgs,
+  hre: HardhatRuntimeEnvironment,
+): Promise<ResolvedFuncArgs> {
+  const { name, func, args, from, to } = taskArgs;
+
+  let contract: ethers.Contract;
+  if (to == "") {
+    const deployment = await hre.deployments.get(name);
+    contract = new hre.ethers.Contract(deployment.address, deployment.abi);
+  } else {
+    contract = await hre.ethers.getContractAt(name, to);
+  }
+
+  const sender = await hre.ethers.getSigner(from);
+  //console.log(await sender.getAddress());
+
+  const unsignedTx = await contract
+    .connect(sender)
+    .populateTransaction[func](...args);
+  //console.log(utx);
+
+  return { contract, sender, unsignedTx };
+}
+
+export interface NormalizeOpts {
+  dec: boolean;
+}
+function normalizeItem(item: any, opts?: NormalizeOpts): any {
+  if (item?.constructor.name == "BigNumber") {
+    if (opts?.dec) {
+      return item.toString();
+    } else {
+      return item.toHexString();
+    }
+  } else {
+    return item;
+  }
+}
+export function normalizeCallResult(res: any, opts?: NormalizeOpts): any {
+  if (_.isArray(res)) {
+    let out = new Array(res.length);
+    for (var i = 0; i < res.length; i++) {
+      out[i] = normalizeItem(res[i], opts);
+    }
+    return out;
+  } else {
+    return normalizeItem(res, opts);
+  }
+}
+export function normalizeRpcResult(obj: any, opts?: NormalizeOpts) {
+  _.forOwn(obj, (val, key) => {
+    obj[key] = normalizeItem(obj[key], opts);
+  });
+  return obj;
 }
