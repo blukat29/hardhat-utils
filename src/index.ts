cb0df0fbc219bdd1ad33aac2fb7f5a91d66835ef
ollie.j <ollie.j@krustuniverse.com>
2023-04-03 10:46:56 +0900

    Check for required plugins

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
-import { extendConfig, extendEnvironment } from "hardhat/config";
-import { lazyObject } from "hardhat/plugins";
-import { HardhatConfig, HardhatUserConfig } from "hardhat/types";
+import { extendEnvironment } from "hardhat/config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { PluginError } from "./helpers";
+
+import "./type-extensions";
+
+extendEnvironment((hre: HardhatRuntimeEnvironment) => {
+  if (!hre.ethers) {
+    throw PluginError("hardhat-ethers plugin not loaded. In your hardhat.config.js, please require or import '@nomiclabs/hardhat-ethers' before hardhat-utils");
+  }
+  if (!hre.deployments) {
+    throw PluginError("hardhat-deploy plugin not loaded. In your hardhat.config.js, please require or import 'hardhat-deploy' before hardhat-utils");
+  }
+});
 
 export * from "./abi";
 export * from "./accounts";
